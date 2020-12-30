d826f21c751d1502b153baa06518963ef926dcb1
AlexandraDamaschin <damaschin.alexandra.maria@gmail.com>
2020-12-30 20:40:41 +0200

    Adding a success flash for signed up users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-
   def show
     @user = User.find(params[:id])
   end
@@ -11,7 +10,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # Handle a successful save
+      flash[:success] = 'Welcome to the Sample App'
+      redirect_to @user
     else
       render 'new'
     end
