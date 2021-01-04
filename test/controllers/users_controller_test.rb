92bb46aef1ac9958431d09f2cd9ae6900bc55cde
AlexandraDamaschin <damaschin.alexandra.maria@gmail.com>
2021-01-04 16:51:47 +0200

    Requiring logged-in users only to acces edit

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class UsersControllerTest < ActionDispatch::IntegrationTest
   def setup
     @base_title = 'Ruby on Rails Tutorial Sample App'
+    @user = users(:alexandra)
   end
 
   test 'should get new' do
@@ -14,5 +15,17 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     get signup_path
     assert_response :success
     assert_select 'title', "Sign up | #{@base_title}"
+  end
+
+  test ' should redirect to edit when not logged in' do
+    get edit_user_path(@user)
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test ' should redirect to update when not logged in' do
+    patch user_path(@user), params: { user: { name: @user.name, email: @user.email } }
+    assert_not flash.empty?
+    assert_redirected_to login_url
   end
 end
