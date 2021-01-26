dd874954aa1d6832ff9699f43246d9a7d1790d2d
AlexandraDamaschin <damaschin.alexandra.maria@gmail.com>
2021-01-26 09:40:05 +0200

    Reset edit action

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 class PasswordResetsController < ApplicationController
+  before_action :get_user, only: [:edit, :update]
+  before_action :valid_user, only: [:edit, :update]
+
   def new
   end
 
@@ -17,5 +20,17 @@ class PasswordResetsController < ApplicationController
   end
 
   def edit
+  end
+
+  private
+
+  def get_user
+    @user = User.find_by(email: params[:email])
+  end
+
+  def valid_user
+    unless @user && @user.activated? && @user.authenticated?(:reset, params[:id])
+      redirect_to root_url
+    end
   end
 end
