9bfad304924c4867f9ba0f1c9711e9d886336180
AlexandraDamaschin <damaschin.alexandra.maria@gmail.com>
2021-01-26 10:06:10 +0200

    Clean up code

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -27,10 +27,9 @@ class PasswordResetsController < ApplicationController
     if params[:user][:password].empty?
       @user.errors.add(:password, "can't be empty")
       render 'edit'
-    elsif @user.update(user_params)
-      @user.forget
-      reset_session
+    elsif @user.update_attributes(user_params)
       log_in @user
+      @user.update_attribute(:reset_digest, nil)
       flash[:success] = 'Password has been reset.'
       redirect_to @user
     else
