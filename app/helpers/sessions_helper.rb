4be7aacbd12a56aa911e68fbce91d75e539a7502
Alexandra Damaschin <damaschin.alexandra.maria@gmail.com>
2021-01-26 08:51:34 +0200

    Account activation (#9)
    * Mailer templates
    
    * Email previews
    
    * Test emails
    
    * Updating the user create action
    
    * Generalizing the auth method
    
    * Activation edit
    
    * Activatation test and refactoring

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -14,7 +14,7 @@ module SessionsHelper
       @current_user ||= user if session[:session_token] == user.session_token
     elsif (user_id = cookies.encrypted[:user_id])
       user = User.find_by(id: user_id)
-      if user&.authenticate?(cookies[:remember_token])
+      if user&.authenticate?(:remember, cookies[:remember_token])
         log_in user
         @current_user = user
       end
