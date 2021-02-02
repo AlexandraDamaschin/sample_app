5d0f5d8df9778ffc534828a84e3fea704cea3862
AlexandraDamaschin <damaschin.alexandra.maria@gmail.com>
2021-02-02 13:56:04 +0200

    Micropost access control

--- test/controllers/microposts_controller_test.rb
+++ test/controllers/microposts_controller_test.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 require 'test_helper'
 
 class MicropostsControllerTest < ActionDispatch::IntegrationTest
@@ -6,6 +7,16 @@ class MicropostsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test ' should redirect create when not logged in' do
-    assert_no_difference 'Micropost.count'
+    assert_no_difference 'Micropost.count' do
+      post microposts_path, params: { micropost: { content: 'Lorem ipsum' } }
+    end
+    assert_redirected_to login_url
+  end
+
+  test 'should redirect destroy when not logged in' do
+    assert_no_difference 'Micropost.count' do
+      delete micropost_path(@micropost)
+    end
+    assert_redirected_to login_url
   end
 end
