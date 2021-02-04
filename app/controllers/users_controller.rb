c14f24fdc608210cd43f933b25e5fe1d0ebbc067
AlexandraDamaschin <damaschin.alexandra.maria@gmail.com>
2021-02-04 10:38:39 +0200

    Basic image upload

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
+# frozen_string_literal: true
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
-  before_action :correct_user, only: [:edit, :update]
+  before_action :logged_in_user, only: %i[index edit update destroy]
+  before_action :correct_user, only: %i[edit update]
   before_action :admin_user, only: :destroy
 
   def index
