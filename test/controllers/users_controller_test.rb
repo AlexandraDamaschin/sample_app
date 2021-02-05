3c775b9f2a2ccff59351aeafcc11503221a5e16b
AlexandraDamaschin <damaschin.alexandra.maria@gmail.com>
2021-02-05 14:54:48 +0200

    Following and Followers pages

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
@@ -76,5 +77,15 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
       delete user_path(@user)
     end
     assert_redirected_to root_url
+  end
+
+  test 'should redirect following when not logged in' do
+    get following_user_path(@user)
+    assert_redirected_to login_url
+  end
+
+  test 'should redirect followers when not logged in' do
+    get followers_user_path(@user)
+    assert_redirected_to login_url
   end
 end
